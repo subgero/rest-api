c091fd41478de26308042727f26a8e4db3bbff78
gero <gcalderon08@gmail.com>
2025-10-05 17:25:14 -0600

    code updated

---
+++ index.js
@@ -0,0 +1,25 @@
+// index.js     
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+
+import userRouters from './routers/userRouters.js';
+
+const app = express();
+
+app.use('/users', userRouters);
+
+app.get('/', (req, res) => {
+    res.send('API is running....');
+});
+
+app.listen(port, (error) => {
+	if(!error)
+		console.log("Server is Successfully Running, and App is listening on port " + port)
+	else
+		console.log("Error occurred, server can't start", error);
+	}
+);
